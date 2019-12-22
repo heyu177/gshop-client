0bd8a80c67156f28dcefdf6161a9d4e1fe83e402
heyu <heyu@qq.com>
2019-12-22 22:46:19 +0800

    ghop_client第二次提交

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,10 @@ import Order from '../pages/Order/Order.vue'
 import Profile from '../pages/Profile/Profile.vue'
 import Search from '../pages/Search/Search.vue'
 import Login from '../pages/Login/Login.vue'
+import Shop from '../pages/Shop/Shop.vue'
+import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods.vue'
+import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo.vue'
+import ShopRatings from '../pages/Shop/ShopRatings/ShopRatings.vue'
 
 //声明使用插件
 Vue.use(VueRouter)
@@ -49,6 +53,28 @@ export default new VueRouter({
         {
             path:'/login',
             component:Login
+        },
+        {
+            path:'/shop',
+            component:Shop,
+            children:[
+                {
+                    path:'/shop/goods',
+                    component:ShopGoods
+                },
+                {
+                    path:'/shop/info',
+                    component:ShopInfo
+                },
+                {
+                    path:'/shop/ratings',
+                    component:ShopRatings
+                },
+                {
+                    path:'',
+                    redirect:'/shop/goods'
+                }
+            ]
         }
     ]
 })
